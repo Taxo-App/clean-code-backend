674209295b64d584d5ac8b6207cd4dce41732359
Sykov7 <bryan@taxo.co>
2023-05-04 11:18:28 -0500

    add new files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        // more files ...
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
